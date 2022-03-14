a8747e24d9f733074434a2bc0bfcdc37ffcb4fc9
DevKobby <fredobiriyeboah@gmail.com>
2022-03-14 02:12:25 +0000

    end of lecture

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import welcome from './welcome.svg';
+import done from './done.png';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1 class="welcome">Welcome to this lecture</h1>
-      <img src={welcome} className="box bounce-7" alt="logo" />
+      <h1 class="welcome">Thanks man</h1>
+      <img src={done} className="box bounce-7" alt="logo" />
 
-      <p class="headline">Learn something cool today</p>
+      <p class="headline">We learnt something new</p>
     </div>
   );
 }
